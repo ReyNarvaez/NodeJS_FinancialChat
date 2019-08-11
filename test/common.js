de1d30bd5d70a10e341e1275b212c5f45b89a3d3
reynarvaez <reynaldo_narvaez@hotmail.com>
2019-08-10 21:38:35 -0600

    added User API tests

---
+++ test/common.js
@@ -0,0 +1,7 @@
+const app = require("../app");
+const mocha = require('mocha');
+const User = require('../models/user');
+
+module.exports = request = require("supertest")(app);
+module.exports = chai = require("chai");
+module.exports = should = chai.should();
