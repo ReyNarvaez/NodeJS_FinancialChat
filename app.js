46d8b3860c0ba7cafc563d4da300dfee06c3a99f
reynarvaez <reynaldo_narvaez@hotmail.com>
2019-08-10 20:19:09 -0600

    added connection to db

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('./dotenvconfig');
+require('./db/mongoose');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
---
+++ db/mongoose.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URL,{
+    useNewUrlParser: true,
+    useCreateIndex: true
+}).then(() =>{
+  console.log('connected to database');
+}).catch(() =>{
+  console.log('failed to connect to database');
+});
