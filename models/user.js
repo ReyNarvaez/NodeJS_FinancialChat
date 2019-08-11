14785799975af4fb9cc0e243697ad441f00903fc
reynarvaez <reynaldo_narvaez@hotmail.com>
2019-08-10 20:22:11 -0600

    added User DB model

---
+++ models/user.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const UserSchema  = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique:true,
+        trim: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid!')
+            }
+        }
+
+    },
+    password:{
+        type:String,
+        required:true,
+        trim:true,
+        minlength: 7,
+        validate(value){
+            if(validator.isEmpty(value)){
+                throw new Error('Please enter your password!')
+            }else if(validator.equals(value.toLowerCase(),"password")){
+                throw new Error('Password is invalid!')
+            }else if(validator.contains(value.toLowerCase(), "password")){
+                throw new Error('Password should not contain password!')
+            }
+        }
+    },
+    tokens:[{
+        token:{
+            type:String,
+            required: true
+        }
+    }],
+    createdAt:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
